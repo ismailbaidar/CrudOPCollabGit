893200531ec541336e4bdd5ba9a66ee6829749a6
Ismail <ismail.baidar198@gmail.com>
2023-12-27 09:20:56 +0100

    Project Initialisation ASP.NET Core

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CrudOPCollabGit.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
