083786e21d100ad5534034b068eb5f92abf4d515
Ismail <ismail.baidar198@gmail.com>
2023-12-27 10:02:22 +0100

    Adding person model

---
+++ Models/Member.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrudOPCollabGit.Models
+{
+    public class Member
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name ="#Id")]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string MemberClub {  get; set; }
+
+    }
+}
